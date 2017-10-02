40fa0572475be8f2f6e8eaea8f85a635923d7b7b
Philippe Simons <philippesimons@Philippes-MBP.lan>
2017-10-02 13:23:03 +0200

    ...

--- android/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-APP_ABI               := armeabi-v7a arm64-v8a x86 x86_64
-APP_MODULES           := vgmplay
-APP_PLATFORM          := android-10
-APP_OPTIM             := release
-NDK_TOOLCHAIN_VERSION := 4.9
